4c8d34282ed420685d439b03840a3f584ecf666d
TrainDoctor <cogdoctor@gmail.com>
2017-03-06 20:52:14 -0800

    Updated correct code

--- BettterToneExample.ino
+++ BettterToneExample.ino
@@ -2,9 +2,9 @@
 #include "BetterTone.h"
 #include "Pitches.h"
 
-//Digital and Analog Pins Used on UNO Board
-const int buzzerPin = 8;    //Digital Pin 8 (No PWM)
-const int buttonPin = 9;	//Digital Pin 9 (PWM)
+//Pins Used on UNO Board
+const int buzzerPin = 8;    //Pin 8 
+const int buttonPin = 9;	//Pin 9
 
 //Default Values for Switch and Button
 int buttonValue = 0;
@@ -19,12 +19,6 @@ void loop(){
 	buttonValue = digitalRead(buttonPin); //The integer buttonValue is used to represent buttonPin as an input
 	
    if(buttonValue == 1){
-// 		Melody(buzzerPin, NCS1, NB1, NAS5, NG7);
-//     Serial.print("4 Tones played to pin number "); Serial.print(buzzerPin); Serial.println(""); Serial.println("");
-// 
-//     DoubleTone(buzzerPin, NCS1, NB1);
-//     Serial.print("2 Tones played to pin number "); Serial.print(buzzerPin); Serial.println(""); Serial.println("");
-  
     BetterTone(buzzerPin, NCS1);
     Serial.print(" 1 Tone played to pin number "); Serial.print(buzzerPin); Serial.println(""); Serial.println("");
    }
